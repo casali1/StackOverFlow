333a40cd6af5f55645cacd6e70217c68fb9eca67
Frank Casali <fcasali@dalandsolutions.com>
2020-01-02 14:35:55 -0700

    Add project files.

---
+++ StackOverFlow/Models/Answer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StackOverFlow.Models
+{
+    public class Answer
+    {
+        public Guid Id { get; set; }
+        public Guid QuestionId { get; set; }
+        public string Body { get; set; }
+    }
+}
---
+++ StackOverFlow/Models/Question.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StackOverFlow.Models
+{
+    public class Question
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public int Score { get; set; }
+        public List<Answer> Answers { get; set; }
+    }
+}
